45461516710cc784773257367af00207c541865b
Lukasz <l.m.ratajczak94@gmail.com>
2020-07-21 19:12:11 +0200

    41.

--- spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -14,7 +14,7 @@ public class SetterDemoApp {
         System.out.println(coach.getDailyFortune());
         System.out.println(coach.getDailyWorkout());
         System.out.println(coach.getEmailAddress());
-        System.out.println(coach.getTeam()-);
+        System.out.println(coach.getTeam());
 
         context.close();
 
