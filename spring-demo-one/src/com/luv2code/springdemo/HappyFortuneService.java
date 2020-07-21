8a01b9c0a79c27e802cc4e49a85b4b4d17c3ab56
Lukasz <l.m.ratajczak94@gmail.com>
2020-07-21 19:24:20 +0200

    42.

--- spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
+++ spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,8 +1,13 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService{
     @Override
     public String getFortune() {
-        return "Today is your lucky day!";
+        Random random = new Random();
+        int i = random.nextInt(3);
+        String[] fortuneArray = {"Today is your lucky day!", "You Rule!", "Nice"};
+        return fortuneArray[i];
     }
 }
