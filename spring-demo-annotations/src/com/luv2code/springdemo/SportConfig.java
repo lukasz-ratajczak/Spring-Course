3395c81cb9ac6a238fc8bd0fdd6e7ef86b72045e
Lukasz <l.m.ratajczak94@gmail.com>
2020-07-22 21:14:53 +0200

    89

--- spring-demo-annotations/src/com/luv2code/springdemo/SportConfig.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SportConfig.java
@@ -1,12 +1,22 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.luv2code.springdemo")
+//@ComponentScan("com.luv2code.springdemo")
 public class SportConfig {
 
+    @Bean
+    public FortuneService sadFortuneService(){
+        return new SadFortuneService();
+    }
 
+    @Bean
+    public Coach swimCoach(){
+        SwimCoach mySwimCoach = new SwimCoach(sadFortuneService());
+        return mySwimCoach;
+    }
 
 }
