fb3ee91d72ef8c04d4999be38b31a0f5b3e6bab6
Lukasz <l.m.ratajczak94@gmail.com>
2020-08-05 19:21:56 +0200

    261

---
+++ hb-05-many-to-many/src/com/luv2code/hibernate/demo/GetCoursesForMaryDemo.java
@@ -0,0 +1,46 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetCoursesForMaryDemo {
+
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start a transaction
+            session.beginTransaction();
+
+            int theId = 2;
+
+            Student tempStudent = session.get(Student.class, theId);
+
+            System.out.println(tempStudent+"\n"+tempStudent.getCourses());
+
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+
+    }
+
+}
