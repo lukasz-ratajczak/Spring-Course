77cb018474540ceaba5297cb9ee6a43ce79595cc
Lukasz <l.m.ratajczak94@gmail.com>
2020-08-17 21:46:23 +0200

    291

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -4,6 +4,7 @@ package com.luv2code.springdemo.controller;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,13 +17,13 @@ import java.util.List;
 @RequestMapping("/customer")
 public class CustomerController {
 
-	@Autowired-
-	CustomerDAO customerDAO;
+	@Autowired
+	private CustomerService customerService;
 
 	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		List<Customer> theCustomers = customerService.getCustomers();
 
 		theModel.addAttribute("customers",theCustomers);
 
