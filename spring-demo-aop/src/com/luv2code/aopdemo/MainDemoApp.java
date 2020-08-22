49701dc34696a587585a2192ce07f311b6cc91a4
Lukasz <l.m.ratajczak94@gmail.com>
2020-08-22 21:06:27 +0200

    324

--- spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo;
 
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,11 +13,14 @@ public class MainDemoApp {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
 
         AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+        MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
 
         accountDAO.addAccount();
-
         accountDAO.addAccount();
 
+        membershipDAO.addAccount();
+        membershipDAO.addAccount();
+        
         context.close();
 
     }
---
+++ spring-demo-aop/src/com/luv2code/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,12 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipDAO {
+
+    public void addAccount(){
+
+        System.out.println(getClass()+": DOING MY STUFF WORK: ADD MEMBERSHIP ACCOUNT");
+    }
+}
