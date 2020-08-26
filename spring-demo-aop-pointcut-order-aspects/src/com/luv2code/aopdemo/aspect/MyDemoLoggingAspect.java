74b317bbb17c5098e34ba30f1649a5914313dba1
Lukasz <l.m.ratajczak94@gmail.com>
2020-08-26 21:41:39 +0200

    337

--- spring-demo-aop-pointcut-order-aspects/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-order-aspects/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,25 +3,17 @@ package com.luv2code.aopdemo.aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 
-    @Before("forDaoPackageNoGetterSetter()")
+    @Before("LuvAopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(){
         System.out.println("\n====>>> Executing @Before advice on addAccount()");
-    }
-
-    @Before("forDaoPackageNoGetterSetter()")
-    public void performApiAnalytics(){
-        System.out.println("\n====> Performing API analytics");
-    }
-
-    @Before("forDaoPackageNoGetterSetter()")
-    public void logToCloudAsync(){
-        System.out.println("\n====> Logging to Cloud in async fashion");
     }
 
 }
