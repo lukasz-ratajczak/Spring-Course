816ccc787300f22830ad77e461d2306048459b18
Lukasz <l.m.ratajczak94@gmail.com>
2020-09-24 18:43:39 +0200

    469

---
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo.rest;
+
+import com.luv2code.springdemo.entity.Student;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+
+        List <Student> students = new ArrayList<>();
+
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Mary", "Smith"));
+
+        return students;
+    }
+}
--- spring-rest-demo/src/main/webapp/index.jsp
+++ spring-rest-demo/src/main/webapp/index.jsp
@@ -7,5 +7,7 @@
 <h3>SPRING REST DEMO</h3>
 <hr>
 <a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+<br><br>
+<a href="${pageContext.request.contextPath}/api/students">Get all Students</a>
 </body>
 </html>
