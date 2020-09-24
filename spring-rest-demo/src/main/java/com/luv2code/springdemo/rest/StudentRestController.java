af80bc1326820f36f3d1c38137963d781dd674af
Lukasz <l.m.ratajczak94@gmail.com>
2020-09-24 18:53:52 +0200

    472

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,9 +3,11 @@ package com.luv2code.springdemo.rest;
 import com.luv2code.springdemo.entity.Student;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,15 +15,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
+    private List<Student> students;
 
-        List <Student> students = new ArrayList<>();
+    @PostConstruct
+    private void loadData() {
+        students = new ArrayList<>();
 
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return students;
     }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        return students.get(studentId);
+    }
 }
+
