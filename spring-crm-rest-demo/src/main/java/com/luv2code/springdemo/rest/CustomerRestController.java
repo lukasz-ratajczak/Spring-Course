bf2609dc964b0e7435de1046278ef309b1c6f61e
Lukasz <l.m.ratajczak94@gmail.com>
2020-09-25 19:16:15 +0200

    502

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,10 +3,7 @@ package com.luv2code.springdemo.rest;
 import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +27,19 @@ public class CustomerRestController {
         }
         return customerService.getCustomer(customerId);
     }
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer){
 
+        customer.setId(0);
+        customerService.saveCustomer(customer);
+
+        return customer;
+    }
+    @PutMapping("/customers")
+    public Customer updateCustomer(@RequestBody Customer customer){
+        customerService.saveCustomer(customer);
+
+        return customer;
+    }
 
 }
