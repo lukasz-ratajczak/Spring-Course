3f843a5a6998a18d0f6d82758dd437e0fee68afa
Lukasz <l.m.ratajczak94@gmail.com>
2020-09-29 18:26:51 +0200

    522

--- mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,17 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: "+coachName+ "  ||  "+ "Team: "+teamName;
+    }
 
     @GetMapping("/")
     public String sayHello() {
