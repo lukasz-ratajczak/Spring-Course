ef818006bdd0971176f7ac7214a3de52c8630848
Lukasz <l.m.ratajczak94@gmail.com>
2020-09-30 16:45:41 +0200

    541

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
